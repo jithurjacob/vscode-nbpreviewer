f101ca23a6f906b41afbf525568ca2b25d72303f
Jithu R Jacob <jithurjacob@gmail.com>
2018-12-04 23:17:36 +0530

    updated to WebView Panel

--- src/extension.js
+++ src/extension.js
@@ -8,117 +8,116 @@ const cheerio = require('cheerio');
 const fileUrl = require("file-url");
 var path = require("path");
 
+const notebook_style_path = vscode.Uri.file(path.join(__dirname, '..', "static", 'notebook.css'));
+const notebook_style_src = notebook_style_path.with({ scheme: 'vscode-resource' });
+
+const prism_style_path = vscode.Uri.file(path.join(__dirname, '..', "static", 'prism.css'));
+const prism_style_src = prism_style_path.with({ scheme: 'vscode-resource' });
+
+const require_script_path = vscode.Uri.file(path.join(__dirname, '..', "static", 'require.js'));
+const require_script_src = require_script_path.with({ scheme: 'vscode-resource' });
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+
+function convertDocToHTML(panel) {
+    statusBarItem.show();
+    statusBarItem.text = "1/6 Starting Rendering";
+    // Get the current text editor
+    let editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        vscode.window.showErrorMessage("Failed to identify editor");
+        statusBarItem.hide();
+        return "Failed to identify editor";
+    }
+
+    let doc = editor.document;
+
+    // Only update status if a Markdown file
+    if (!(doc.languageId === 'ipython')) {
+        vscode.window.errorMessage("Active editor doesn't show a IPython notebook - cannot preview.");
+        statusBarItem.hide();
+        return "Active editor doesn't show a IPython notebook - cannot preview.";
+    }
+    var data = "";
+    try {
+        statusBarItem.text = "2/6 Extracting IPython Notebook";
+        var text = doc.getText();
+        var ipynb = JSON.parse(text);
+        statusBarItem.text = "3/6 Parsing IPython Notebook";
+        var notebook = nb.parse(ipynb);
+        statusBarItem.text = "4/6 Rendering IPython Notebook";
+        var notebook_html = notebook.render().outerHTML;
+        statusBarItem.text = "5/6 Highlighting IPython Notebook";
+        //traverse through notebook and use prism to highlight
+        const $ = cheerio.load(notebook_html);
+        var elems = $('.nb-input pre code');
+        for (var i = 0; i < elems.length; i++) {
+            var formatted_text = Prism.highlight($(elems[i]).text(), Prism.languages.python);
+            $(elems[i]).html(formatted_text);     // modify inner HTML
+        }
+        notebook_html = $.html();
+        data = '<link  href="' + notebook_style_src + '"  rel="stylesheet" />';
+        data += '<link href="' + prism_style_src + '"  rel="stylesheet" />';
+        data += '<script src="' + require_script_src + '" ></script>';
+        data += "<body>" + notebook_html + "</body>";
+    } catch (error) {
+        data = "An error occured while converting Notebook to HTML";
+        vscode.window.showErrorMessage("An error occured while converting Notebook to HTML");
+        console.error("An error occured while converting Notebook to HTML", error);
+    }
+    statusBarItem.hide();
+    return data;
+}
+
+function generatePreview(panel) {
+    var html_body = convertDocToHTML(panel);
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Preview</title>
+    </head>`+ html_body + `</html>`;
+}
+
+function generateProgressMessage(message) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Preview</title>
+    </head><body>`+ message + `</body></html>`;
+}
+
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "nbpreviewer" is now active!');
 
-
-    // create the document content provider
-    var previewUri = vscode.Uri.parse('nb-preview://authority/nb-preview');
-    var TextDocumentContentProvider = /** @class */ (function () {
-        function TextDocumentContentProvider() {
-            this._onDidChange = new vscode.EventEmitter();
-        }
-        TextDocumentContentProvider.prototype.provideTextDocumentContent = function (uri) {
-            return this.createIPythonDocument();
-        };
-        Object.defineProperty(TextDocumentContentProvider.prototype, "onDidChange", {
-            get: function () {
-                return this._onDidChange.event;
-            },
-            enumerable: true,
-            configurable: true
-        });
-        TextDocumentContentProvider.prototype.update = function (uri) {
-            this._onDidChange.fire(uri);
-        };
-        TextDocumentContentProvider.prototype.createIPythonDocument = function () {
-            var editor = vscode.window.activeTextEditor;
-            if (!(editor.document.languageId === 'ipython')) {
-                return this.errorMessage("Active editor doesn't show a IPython notebook - cannot preview.");
-            }
-            return this.extractIPythonBody();
-        };
-        TextDocumentContentProvider.prototype.extractIPythonBody = function () {
-            var editor = vscode.window.activeTextEditor;
-            return this.convertToHTML(editor.document);
-
-        };
-        TextDocumentContentProvider.prototype.errorMessage = function (error) {
-            return "<body>" + error + "</body>";
-        };
-        TextDocumentContentProvider.prototype.convertToHTML = function (document) {
-            var data = "";
-            try {
-                var text = document.getText();
-                var ipynb = JSON.parse(text);
-                var notebook = nb.parse(ipynb);
-                var notebook_html = notebook.render().outerHTML;
-                //traverse through notebook and use prism to highlight
-                const $ = cheerio.load(notebook_html);
-                var elems = $('.nb-input pre code');
-                for (var i = 0; i < elems.length; i++) {
-                    var formatted_text = Prism.highlight($(elems[i]).text(), Prism.languages.python);
-                    $(elems[i]).html(formatted_text);     // modify inner HTML
-                }
-                notebook_html = $.html();
-
-                var notebook_style_path = fileUrl(
-                    path.join(
-                        __dirname,
-                        "..",
-                        "static",
-                        "notebook.css"
-                    ));
-                var prism_style_path = fileUrl(path.join(
-                    __dirname,
-                    "..",
-                    "static",
-                    "prism.css"
-                ));
-
-                var require_script_path = fileUrl(path.join(
-                    __dirname,
-                    "..",
-                    "static",
-                    "require.js"
-                ));
-
-                data = '<link  href="' + notebook_style_path + '"  rel="stylesheet" />';
-                data += '<link href="' + prism_style_path + '"  rel="stylesheet" />';
-                data += '<script src="' + require_script_path + '" ></script>';
-                data += "<body>" + notebook_html + "</body>";
-            } catch (error) {
-                data = this.errorMessage(error);
-                console.error("An error occured while converting Notebook to HTML", error);
-            }
-            return data;
-        };
-        return TextDocumentContentProvider;
-    }());
-    var provider = new TextDocumentContentProvider();
-    var registration = vscode.workspace.registerTextDocumentContentProvider('nb-preview', provider);
-
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.showPreview', function (obj) {
-
-        console.log("showing preview for", previewUri);
-        provider.update(previewUri);
+    let disposable = vscode.commands.registerCommand('extension.showPreview', async function (obj) {
         // The code you place here will be executed every time your command is executed
-        return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two, 'IPython Notebook Preview').then(function (success) {
+        try {
+            // const success = await vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two, 'IPython Notebook Preview');
+            // Create and show panel
+            const panel = vscode.window.createWebviewPanel('nbpreviewer', "Jupyter Notebook Previewer", vscode.ViewColumn.One, { enableScripts: true });
+            panel.webview.html = generateProgressMessage("Starting to render Jupyter Notebook");
+            // And set its HTML content
+            panel.webview.html = generatePreview(panel);
             console.log("successfully showed notebook");
-        }, function (reason) {
-            vscode.window.showErrorMessage("An error occured while rendering the Notebook", reason);
-        });
+        }
+        catch (reason) {
+            console.error(reason);
+            vscode.window.showErrorMessage("An error occured while rendering the Notebook");
+        }
     });
 
-    context.subscriptions.push(disposable, registration);
+    context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
